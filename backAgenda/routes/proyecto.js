5bf456ae9bbdd71326afa3d8648179798cf46c49
gianca243 <gianca243@gmail.com>
2020-11-26 18:25:50 -0500

    Casi todo
    Co-authored-by: zer0th <hernanmog@gmail.com>

--- backAgenda/routes/proyecto.js
+++ backAgenda/routes/proyecto.js
@@ -1,14 +1,42 @@
 const { request } = require("express");
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth")
 //Módulos internos
 const { Proyecto } = require("../model/proyecto");
 const { Usuario } = require("../model/usuario");
 const { Rol } = require("../model/rol");
 //Ruta
-router.post("/",async()=>{
-  const proyecto = await Proyecto.findOne({
-    key: 'value'
-  });
-  
+router.post("/crearProyecto",auth,async()=>{
+  const usuario = await Usuario.findById(req.usuario._id)
+  if(!usuario) return res.status(401).send("usuario no existe en DB")
+  const rol = await Rol.find({
+    id_usuario:usuario._id
+  }) 
+  rol.map(async function(v){
+    const proyecto = await Proyecto.findOne({
+      id_proyecto: v.id_proyecto
+    });
+    if(proyecto.nombre_proyecto==req.body.nombre_proyecto){
+      return proyecto
+    }
+  })
+  if(rol.length>0){
+    return res.status(401).send("ya existe el proyecto")
+  }
+  const proyectos = new Proyecto({
+    nombre_proyecto:req.body.nombre_proyecto,
+    estado:"activo"
+  })
+  const roles = new Rol({
+    id_usuario:usuario._id,
+    id_proyecto: proyectos._id,
+    rol:"admin"
+  })
+  const result = await proyectos.save()
+  res.status(200).send(result)
+  const result2 = await roles.save()
+  res.status(200).send(result2)  
 })
+
+module.exports = router;
--- frontAgenda/src/app/app.module.ts
+++ frontAgenda/src/app/app.module.ts
@@ -7,6 +7,34 @@ import { CabeceraComponent } from './cabecera/cabecera.component';
 import { SliderComponent } from './slider/slider.component';
 import { MarketingComponent } from './marketing/marketing.component';
 import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
+import { HomeComponent } from './home/home.component';
+import { InvitationComponent } from './auth/invitation/invitation.component';
+import { TaskComponent } from './auth/task/task.component';
+import { SidebarComponent } from './comp/sidebar/sidebar.component';
+//complementos
+import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+//componentes
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatButtonModule} from '@angular/material/button';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet'; 
+import {MatListModule} from '@angular/material/list'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import { ToolbarComponent } from './comp/toolbar/toolbar.component'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateProjectComponent } from './auth/create-project/create-project.component';
+import { CalendarComponent } from './auth/calendar/calendar.component';
+import { EditarPerfilComponent } from './auth/editar-perfil/editar-perfil.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { UserListComponent } from './auth/user-list/user-list.component'; 
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -14,11 +42,31 @@ import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
     CabeceraComponent,
     SliderComponent,
     MarketingComponent,
-    PiePaginaComponent
+    PiePaginaComponent,
+    HomeComponent,
+    InvitationComponent,
+    TaskComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    CreateProjectComponent,
+    CalendarComponent,
+    EditarPerfilComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatBottomSheetModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    BrowserModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
